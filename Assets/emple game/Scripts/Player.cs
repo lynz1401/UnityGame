a3531f36981c53445b74b1c64c57e86525041dc1
linzi <purplecookie1401@gmail.com>
2018-05-14 10:27:58 +0100

    k

--- Assets/emple game/Scripts/Player.cs
+++ Assets/emple game/Scripts/Player.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	public string logText = "Hello World Again" ; 
+
 	// Use this for initialization
 	void Start () {
-		Debug.Log("Hello World!");
+		 
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        Debug.Log(logText);
+		Rigidbody2D rigidbody = GetComponent<Rigidbody2D>(); 
+		rigidbody.velocity = Vector2.right;
+
+    } 
 }
